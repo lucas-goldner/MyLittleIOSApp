131d6e3992f8a40a3915723124c9d21519916efa
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-12 01:09:06 +0100

    Initial Commit

---
+++ Shared/MyLittleIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLittleIOSAppApp.swift
+//  Shared
+//
+//  Created by Lucas Goldner on 12.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MyLittleIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
