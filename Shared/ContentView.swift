64d6b320757056c8a24be0936b30fd38a5366972
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-12 01:44:27 +0100

    Playing with modifiers

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, Lucas!")
-            .padding()
+            .padding(.all).background(Color.orange).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/).border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
+        
     }
 }
 
