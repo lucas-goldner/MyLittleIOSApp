8f92b15e8a88ade2c1e55617d3f3afe778a10836
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-12 22:15:10 +0100

    Multiple Elements in a stack

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Lucas!")
-            .padding(.all).background(Color.orange).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/).border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
+        
+    VStack {
+        Text("My little IOS App")
+        
+        Image("L")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        
+    }
         
     }
 }
