08f186efd6637d840334e13b5f1f4adb3d39b146
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-12 22:58:56 +0100

    Working with functions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -12,3 +12,13 @@ print(myInt)
 
 let myConst:String = "I cannot be changed"
 print(myConst)
+
+let a = 10
+let b = 20
+
+func myFunc(a: Int, b: Int) -> Int{
+    return a + b
+}
+
+let result = myFunc(a: a, b: b)
+print(result)
